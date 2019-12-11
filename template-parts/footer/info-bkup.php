91f5f6b394d1bf11cb568c21a46af39b4f403eb1
Daniel Bisett <daniel@austintatiousdesign.co>
2019-12-10 19:56:05 -0600

    v2.0.2 initial component items added

--- template-parts/footer/info-bkup.php
+++ template-parts/footer/info-bkup.php
@@ -10,16 +10,16 @@ namespace WP_Rig\WP_Rig;
 ?>
 
 <div class="site-info">
-	<a href="<?php echo esc_url( __( 'https://austintatiousdesign.co/', 'wp-rig' ) ); ?>">
+	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wp-rig' ) ); ?>">
 		<?php
 		/* translators: %s: CMS name, i.e. WordPress. */
-		printf( esc_html__( 'Proudly built and maintained by %s', 'wp-rig' ), 'Austintatious Design' );
+		printf( esc_html__( 'Proudly powered by %s', 'wp-rig' ), 'WordPress' );
 		?>
 	</a>
 	<span class="sep"> | </span>
 	<?php
 	/* translators: Theme name. */
-	printf( esc_html__( 'Theme: %s by Daniel Bisett.', 'wp-rig' ), '<a href="' . esc_url( 'https://github.com/austintude/wflag.git' ) . '">WFLAG Org</a>' );
+	printf( esc_html__( 'Theme: %s by the contributors.', 'wp-rig' ), '<a href="' . esc_url( 'https://github.com/wprig/wprig/' ) . '">WP Rig</a>' );
 
 	if ( function_exists( 'the_privacy_policy_link' ) ) {
 		the_privacy_policy_link( '<span class="sep"> | </span>' );
