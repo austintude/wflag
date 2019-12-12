4d865453fcb1b062daa0213b8be3ce6fe7f06236
Daniel Bisett <daniel@austintatiousdesign.co>
2019-12-12 08:22:41 -0600

    v2.0.5 updated fonts and custom-logo in footer

--- template-parts/footer/info.php
+++ template-parts/footer/info.php
@@ -10,7 +10,13 @@ namespace WP_Rig\WP_Rig;
 ?>
 
 <div class="site-info">
-	<a href="<?php echo esc_url( __( 'https://austintatiousdesign.co/', 'wp-rig' ) ); ?>">
+<div class="baseBlock">
+
+<div id="themeLogo" aria-label="custom logo link">
+<?php the_custom_logo(); ?>
+</div>
+<div class="builtBy">
+<a href="<?php echo esc_url( __( 'https://austintatiousdesign.co/', 'wp-rig' ) ); ?>">
 		<?php
 		/* translators: %s: CMS name, i.e. WordPress. */
 		printf( esc_html__( 'Proudly built and maintained by %s', 'wp-rig' ), 'Austintatious Design' );
@@ -25,4 +31,7 @@ namespace WP_Rig\WP_Rig;
 		the_privacy_policy_link( '<span class="sep"> | </span>' );
 	}
 	?>
+</div>
+</div>
+
 </div><!-- .site-info -->
